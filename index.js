801d4df238940f14cb39691f4829111898c3a21c
baobaodz <3208417157@qq.com>
2024-07-28 23:35:34 +0800

    update

--- index.js
+++ index.js
@@ -2,31 +2,97 @@
 const fs = require('fs');
 const path = require('path');
 
+const pkg = require("./package.json");
+const config = require("./lib/config")(hexo);
+const copyFile = require("./lib/utils").copyFile.bind(hexo);
+
+
+let cssHref = `/css/${pkg.name}.css`;
+let jsSrc = `/js/${pkg.name}.js`;
+if (config.cdn) {
+    cssHref = `https://cdn.jsdelivr.net/npm/${pkg.name}@${pkg.version}/css/index.css`;
+    jsSrc = `https://cdn.jsdelivr.net/npm/${pkg.name}@${pkg.version}/js/index.js`;
+} else {
+    copyFile(`${pkg.name}-css`, cssHref, path.join(__dirname, "./css/index.css"));
+    copyFile(`${pkg.name}-js`, jsSrc, path.join(__dirname, "./js/script.js"));
+}
+
+const linkTag = `<link href="${cssHref}" rel="stylesheet"/>`;
+const scriptTag = `<script src="${jsSrc}"></script>`;
+
+
+/**
+ * 插入 html 页面
+ * @param {string} layout
+ */
+function insertToHtml(layout) {
+    console.log('insertToHtml layout: ', layout);
+    hexo.extend.generator.register("mbti-card", function(locals) {
+        console.log('insertToHtml option: ', config);
+
+        const mbtiContainer = `<div class="mbti-card" id="mbti-container" style="width: 600px;margin: 0 auto"></div>`
+
+        const script = `
+        <script>
+            const container = document.getElementById('mbti-container');
+            const mbtiDimensions = getMBTIDimensions(${JSON.stringify(config)});
+            container.innerHTML = getMBTIBars(mbtiDimensions);
+            console.log('insertToHtml slide: ', ${config.slide});
+            addSliderListeners(mbtiDimensions, ${config.slide});
+        </script>
+        `;
+        const mbtiContent = mbtiContainer + script;
+        hexo.extend.injector.register(
+            "body_end",
+            mbtiContent,
+            layout
+        );
+    });
+    hexo.extend.injector.register("head_begin", linkTag, layout);
+    hexo.extend.injector.register("body_end", scriptTag, layout);
+
+    if (config.hide) {
+        hexo.extend.injector.register(
+            "body_end",
+            "<style>#widget-tree-button{opacity:0}</style>",
+            layout
+        );
+    }
+}
+
+if (Array.isArray(config.layout)) {
+    config.layout.forEach((layout) => {
+        insertToHtml(layout);
+    });
+} else {
+    insertToHtml(config.layout);
+}
+
 // 插件初始化
 hexo.extend.filter.register('after_generate', function(data) {
     if (hexo.config.mbtiData) {
-        const { introversion, extraversion, sensing, intuition, feeling, thinking, judging, perceiving, assertiveness, fluctuation } = hexo.config.mbtiData;
+        // const { introversion, extraversion, sensing, intuition, feeling, thinking, judging, perceiving, assertiveness, fluctuation } = hexo.config.mbtiData;
 
-        const mbtiContainer = `<div class="mbti-card" id="mbti-container">
+        // const mbtiContainer = `<div class="mbti-card" id="mbti-container">
 
-        </div>`;
-        const scriptContent = fs.readFileSync(path.join(__dirname, 'script.js'), 'utf8');
-        const cssContent = fs.readFileSync(path.join(__dirname, 'styles.css'), 'utf8');
+        // </div>`;
+        // const scriptContent = fs.readFileSync(path.join(__dirname, 'script.js'), 'utf8');
+        // const cssContent = fs.readFileSync(path.join(__dirname, 'styles.css'), 'utf8');
 
-        const script = `
-             <script>
-                ${scriptContent}
-             </script>
-        `;
-        const css = `
-            <style>
-              ${cssContent}
-            </style>
-        `;
+        // const script = `
+        //      <script>
+        //         ${scriptContent}
+        //      </script>
+        // `;
+        // const css = `
+        //     <style>
+        //       ${cssContent}
+        //     </style>
+        // `;
 
-        const mbtiContent = mbtiContainer + script + css;
+        // const mbtiContent = mbtiContainer + script + css;
 
-        // 将内容注入到about页面
-        hexo.extend.injector.register('body_end', mbtiContent, 'about');
+        // // 将内容注入到about页面
+        // hexo.extend.injector.register('body_end', mbtiContent, 'about');
     }
 });
--- js/script.js
+++ js/script.js
@@ -1,18 +1,10 @@
-const mbtiDimensions = [
-    { id: 'ei', label: ['外向', '内向'], value: [40, 60], color: '#4298b4' },
-    { id: 'sn', label: ['有远见', '现实'], value: [50, 50], color: '#e4ae3a' },
-    { id: 'tf', label: ['理性分析', '感受'], value: [50, 50], color: '#33a474' },
-    { id: 'jp', label: ['评判', '展望'], value: [50, 50], color: '#88619a' },
-    { id: 'at', label: ['坚决', '起伏不定'], value: [50, 50], color: '#f25e62' },
-];
-
 function createBarHTML(dimension) {
     return `
       </div>
         <div class="trait-container">
             <span class="trait-left">${dimension.label[0]}</span>
             <div class="slider-container">
-                <input type="range" min="0" max="100" value="${dimension.value[0]}" class="slider" id="${dimension.id}Slider">
+                <input type="range" min="0" max="100" value="${dimension.value[0]}" class="slider" id="${dimension.id}Slider" disabled>
                 <div id="${dimension.id}SliderValue" class="slider-value"></div>
             </div>
             <span class="trait-right">${dimension.label[1]}</span>
@@ -20,12 +12,19 @@ function createBarHTML(dimension) {
     `;
 }
 
-function renderMBTIBars() {
-    const container = document.getElementById('mbti-container');
-    const barsHTML = mbtiDimensions.map(createBarHTML).join('');
-    container.innerHTML = barsHTML;
+function getMBTIDimensions(config) {
+    return [
+        { id: 'ei', label: ['外向', '内向'], value: config.data['E-I'] || config.data['外向-内向'], color: config.color[0] },
+        { id: 'sn', label: ['有远见', '现实'], value: config.data['S-N'] || config.data['有远见-现实'], color: config.color[1] },
+        { id: 'tf', label: ['理性分析', '感受'], value: config.data['T-F'] || config.data['理性分析-感受'], color: config.color[2] },
+        { id: 'jp', label: ['评判', '展望'], value: config.data['J-P'] || config.data['评判-展望'], color: config.color[3] },
+        { id: 'at', label: ['坚决', '起伏不定'], value: config.data['A-T'] || config.data['坚决-起伏不定'], color: config.color[4] },
+    ];
 }
-renderMBTIBars();
+
+function getMBTIBars(mbtiDimensions) {
+    return mbtiDimensions.map(createBarHTML).join('');
+}
 
 function updateSliderValue(value, dimension, slider, sliderValue) {
     console.log('offsetWidth: ', slider.offsetWidth)
@@ -35,10 +34,10 @@ function updateSliderValue(value, dimension, slider, sliderValue) {
     const position = (value / 100) * slider.offsetWidth;
     sliderValue.style.left = `${position}px`;
 
-    // sliderValue.style.transform = `translateX(${value}%)`;
 }
 
-function addSliderListeners() {
+function addSliderListeners(mbtiDimensions, slide) {
+    console.log('slide: ', slide);
     mbtiDimensions.forEach(dimension => {
         const slider = document.getElementById(`${dimension.id}Slider`);
         const sliderValue = document.getElementById(`${dimension.id}SliderValue`);
@@ -50,14 +49,16 @@ function addSliderListeners() {
         slider.dispatchEvent(event);
 
         updateSliderValue(slider.value, dimension, slider, sliderValue);
-        slider.addEventListener('input', (e) => {
-            console.log(e.target.value);
 
-            updateSliderValue(slider.value, dimension, slider, sliderValue);
+        if (slide) {
+            slider.removeAttribute('disabled');
+            slider.addEventListener('input', (e) => {
+                console.log(e.target.value);
+
+                updateSliderValue(slider.value, dimension, slider, sliderValue);
 
 
-        });
+            });
+        }
     });
 }
-
-addSliderListeners();
